1b901e61f14cd01c26d8fc21e14a466ae8772dd7
Tahcin Sarwar <88557978+tahcin@users.noreply.github.com>
2025-04-25 12:17:42 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -45,7 +45,7 @@ function initializeTimers() {
     const event3Date = new Date("March 19, 2025 23:30:00").getTime();
     const event4Date = new Date("March 26, 2025 23:30:00").getTime();
     const event5Date = new Date("April 20, 2025 18:30:00").getTime();
-    const event6Date = new Date("April 22, 2025 23:30:00").getTime();
+    const event6Date = new Date("May 2, 2025 23:30:00").getTime();
     const event7Date = new Date("April 16, 2025 23:30:00").getTime();
     const event8Date = new Date("April 16, 2025 23:30:00").getTime();
     
